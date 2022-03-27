ebf8e7a33701f85fa9a46a27b902b91f95e92e15
Nguyễn Minh Trường <truong.nguyen@difisoft.com>
2022-03-27 16:53:43 +0700

    Auth dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -9,7 +10,7 @@ export class AuthService {
     return { msg: 'Signed in' };
   }
 
-  signup() {
+  signup(dto: AuthDto) {
     return { msg: 'Signed up' };
   }
 }
