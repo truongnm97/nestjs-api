a38b9dbf2ec8747081add98ae66e78dd7a9c578b
Nguyễn Minh Trường <truong.nguyen@difisoft.com>
2022-03-27 22:40:50 +0700

    Update user api

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,13 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from 'app.module';
+import { PrismaService } from 'prisma/prisma.service';
+import * as pactum from 'pactum';
+import { EditUserDto } from 'user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeAll(async () => {
     const modulteRef = await Test.createTestingModule({
@@ -16,11 +20,84 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    await app.listen(5000);
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:5000');
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('should display welcome message');
+  describe('Auth', () => {
+    const dto = {
+      email: 'hello@gmail.com',
+      password: '123456',
+    };
+
+    describe('Sign Up', () => {
+      it('should sign up a new user', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Sign In', () => {
+      it('should sign in', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200);
+      });
+    });
+    describe('Edit user', () => {
+      const dto: EditUserDto = {
+        firstName: 'Hello',
+        email: 'hello@gmail.com',
+      };
+      it('should edit user', () => {
+        return pactum
+          .spec()
+          .patch('/users')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .withBody(dto)
+          .expectStatus(200)
+          .expectBodyContains(dto.email)
+          .expectBodyContains(dto.firstName)
+          .inspect();
+      });
+    });
+  });
+
+  describe('Bookmark', () => {
+    describe('Create bookmark', () => {});
+    describe('Get bookmark', () => {});
+    describe('Get bookmark by id', () => {});
+    describe('Edit bookmark by id', () => {});
+    describe('Delete bookmark by id', () => {});
+  });
 });
